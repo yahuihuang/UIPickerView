c0a14efc72fb2d1792c7270095bf0ee20c1d6c55
Huang Grace <huang.yahui.grace@gmail.com>
2019-11-16 16:45:14 +0800

    select astrological & bloudType

--- UIPickerView/ViewController.swift
+++ UIPickerView/ViewController.swift
@@ -8,13 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    var astrological = ["請選擇你的星座","白羊宮","金牛宮","雙子宮","巨蟹宮","獅子宮","處女宮","天秤宮","天蠍宮","射手宮","摩羯宮","水瓶宮","雙魚宮"]
+    var bloudType = ["請選擇你的血型","A","B","O","AB"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pickerView.dataSource = self
+        pickerView.delegate = self
     }
-
-
+    
+    //MARK: PickerView DataSource & Delegate [UIPickerViewDataSource protocol]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return astrological.count
+        case 1:
+            return bloudType.count
+        default:
+            return 0
+        }
+    }
+    
+    //MARK: PickerView DataSource & Delegate [UIPickerViewDelegate protocol]
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component {
+        case 0:
+            return astrological[row]
+        case 1:
+            return bloudType[row]
+        default:
+            return nil
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            print(astrological[row])
+        case 1:
+            print(bloudType[row])
+        default:
+            break
+        }
+    }
 }
 
